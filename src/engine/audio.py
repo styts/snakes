17d74ade654873bf02c5e4e0cc74d16f02539428
styts <github@styts.com>
2012-08-12 11:40:17 +0200

    sfx now on channel = no echos in sfx

--- src/engine/audio.py
+++ src/engine/audio.py
@@ -1,7 +1,18 @@
+import pygame
+
+"""
+Channels:
+0 : default sound effects
+"""
+
 class AudioManager:
 	def __init__(self, app):
 		self.app = app
+		self.channels = []
+		c = pygame.mixer.Channel(0)
+		self.channels.append(c)
 
-	def sfx(self, name):
+	def sfx(self, name, channel_nr=0):
 		sound = self.app.resman.get_sound(name)
-		sound.play()
+		channel = self.channels[channel_nr]
+		channel.play(sound)
