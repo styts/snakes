108c702bd30cd145a7571de4edd539596d0090c0
styts <github@styts.com>
2012-08-15 16:12:53 +0200

    Mac.app works!

--- assets/build.py
+++ assets/build.py
@@ -4,22 +4,25 @@ import os
 import sys
 
 pfm = sys.platform
+os.chdir(os.path.dirname(__file__))
 
+# build
 path2build = {}
 path2build["darwin"] = "~/bin/pyinstaller/utils/"
 path2build["win32"] = "T:\\bin\\pyinstaller\\utils\\"
 
-sep = {}
-sep['darwin'] = '/'
-sep['win32'] = '\\'
-
-os.chdir(os.path.dirname(__file__))
-
-buildpath = "\"%s\" \"%s\"" % (os.path.join("..", "build", "pyi.%s" % pfm),
-                       "build.spec")
-
-command = "python %sBuild.py --noconfirm --buildpath=%s" % (path2build[pfm], buildpath)
-
-#print command
+command = "python %sBuild.py --noconfirm %s.spec" % (path2build[pfm], pfm)
 
 os.system(command)
+
+# distribute
+os.system("mkdir -p %s" % os.path.join("..", 'dist'))
+if pfm == 'darwin':
+    os.system("rm -rf ../dist/Snakes.app")
+    os.system("mv dist/Snakes.app ../dist")
+else:
+    os.system("rm -rf ..\\dist\\Snakes")
+    os.system("mv dist\\Snakes ..\\dist")
+
+# cleanup
+os.system("rm -rf build dist logdict*.log")
